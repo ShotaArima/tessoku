b348ae3edea8c4f1cd05d8f2edd2bf1fe97fa076
ShotaArima <g2172010@tcu.ac.jp>
2024-02-29 03:19:57 +0900

    complete B03

--- B/B03.py
+++ B/B03.py
@@ -4,7 +4,11 @@ ans=False
 
 for i in range(n):
     for j in range(n):
+        if i==j:
+            continue
         for k in range(n):
+            if k==i or k==j:
+                continue
             sum = a[i]+a[j]+a[k]
             if sum==1000:
                 ans=True
